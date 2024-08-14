11c9882a167d181c2dd1d5f9350607c445a20f9e
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-08-14 19:11:33 -0300

    Chore: Input external dependencies

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,15 @@
+import type { AnswerRepository } from './../repositories/answers-repository'
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
 
-test('Create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswerRepository = {
+  create: async () => {},
+}
 
-  const answer = answerQuestion.execute({
+test('Create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: 'questionId-1',
     content: 'nova resposta',
     authorId: 'instructorId-1',
