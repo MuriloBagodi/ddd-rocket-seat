c19fff59c426709634935f37d720942ee7f7dc9b
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-16 17:45:40 -0300

    Feat: Adding porps to our business rules.

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,11 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 interface AnswerProps {
   content: string
-  authorId: string
-  questionId: string
-  authorRole: string
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from './value-objects/slug'
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
   title: string
   content: string
-  authorId: string
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   slug: Slug
+  cratedAt: Date
+  updatedAt: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
