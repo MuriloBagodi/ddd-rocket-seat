448b8127b23ef3d9c99587daecb23c77350ff6f3
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-16 17:39:09 -0300

    Feat: Getters and Setters POO

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,22 +1,12 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entity'
 interface AnswerProps {
   content: string
   authorId: string
   questionId: string
   authorRole: string
 }
-export class Answer {
-  public id: string
-  public content: string
-  public questionId: string
-  public authorId: string
-  public authorRole: string
-
-  constructor(props: AnswerProps, id?: string) {
-    this.content = props.content
-    this.authorId = props.authorId
-    this.questionId = props.questionId
-    this.authorRole = props.authorRole
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from 'node:crypto'
 import type { Slug } from './value-objects/slug'
+import { Entity } from '../../core/entities/entity'
 
 interface QuestionProps {
   title: string
@@ -8,19 +8,4 @@ interface QuestionProps {
   slug: Slug
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public content: string
-  public slug: Slug
-  public authorId: string
-
-  constructor(props: QuestionProps, id?: string) {
-    // Here we can use some of JS to Assign the objects using Object.assign(this, props)
-    this.title = props.title
-    this.content = props.content
-    this.authorId = props.authorId
-    this.slug = props.slug
-    this.id = id ?? randomUUID()
-  }
-}
+export class Question extends Entity<QuestionProps> {}
